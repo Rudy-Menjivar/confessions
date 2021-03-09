9829546187f0643c44c0161e0be6fc0134fca9a6
Greg Stinson <gstinsonjr17@gmail.com>
2021-03-08 21:05:56 -0800

    jumbotron js file imports react and has function layout

---
+++ client/src/components/Jumbotron/index.js
@@ -0,0 +1,12 @@
+//import react and  styling file
+import React from "react";
+import "./style.css";
+
+//create function for jumbotron 
+function Jumbotron(){
+    return (
+        // jumbotron html here
+    );
+}
+
+export default Jumbotron;
