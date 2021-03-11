ad4c84ee2d5a1c1410764afd507525529cff90de
Uyen Nguyen <udnguyen@ucsd.edu>
2021-03-10 20:01:29 -0800

    add mongoose connectionin seedDB

--- scripts/seedDB.js
+++ scripts/seedDB.js
@@ -2,7 +2,8 @@ const mongoose = require("mongoose");
 const db = require("../models");
 
 mongoose.connect(
-    
+  process.env.MONGODB_URI ||
+  "mongodb://localhost/confessions"
 )
 const confessionSeed = [
 {
