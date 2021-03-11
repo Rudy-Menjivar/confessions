087facfd1dbe6684a35c8d03f22258fb58c6ef21
Greg Stinson <gstinsonjr17@gmail.com>
2021-03-10 19:18:19 -0800

    Added seeds for db

--- scripts/seedDB.js
+++ scripts/seedDB.js
@@ -6,5 +6,40 @@ mongoose.connect(
 )
 
 const confessionSeed = [
-// create your seeds here
+{
+    title: "I skipped the gym",
+    user: "gymLover101",
+    synopsis: "Gym",
+    date: new Date(Date.now())
+},
+{
+    title: "I cheated in school",
+    user: "collegeKid101",
+    synopsis: "School",
+    date: new Date(Date.now())
+},
+{
+    title: "I lied to my mom",
+    user: "badKid101",
+    synopsis: "Lies",
+    date: new Date(Date.now())
+},
+{
+    title: "I skipped class",
+    user: "harvardBound101",
+    synopsis: "School",
+    date: new Date(Date.now())
+}
 ];
+
+db.Confession
+  .remove({})
+  .then(() => db.Confession.collection.insertMany(confessionSeed))
+  .then(data => {
+    console.log(data.result.n + " records inserted!");
+    process.exit(0);
+  })
+  .catch(err => {
+    console.error(err);
+    process.exit(1);
+  });
