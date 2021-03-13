3842bf06cba101e56e9c0ba1347f7e7263b31753
Uyen Nguyen <udnguyen@ucsd.edu>
2021-03-12 17:00:33 -0800

    add controller

--- controllers/confessionController.js
+++ controllers/confessionController.js
@@ -5,18 +5,34 @@ const db = require("../models");
 module.exports = {
     findAll: function (req, res) {
         db.Confession
-        
+        .find(req.query)
+        .then(dbAllConfession => res.json(dbAllConfession))
+        .catch(err => 
+            res.status(422).json(err));
     },
     findById: function (req, res) {
         db.Confession
+        .findById(req.params.id)
+        .then(dbOneConfession => res.json(dbOneConfession))
+        .catch(err => res.status(422).json(err));
     },
     create: function (req, res) {
         db.Confession
+        .create(req.body)
+        .then(dbNewConfession => res.json(dbNewConfession))
+        .catch(err => res.status(422).json(err));
     },
     update: function (req, res) {
         db.Confession
+        .findOneAndUpdate({ _id: req.params.id }, req.body)
+        .then(dbUpdatedConfession => res.json(dbUpdatedConfession))
+        .catch(err => res.status(422).json(err));
     },
     remove: function (req, res) {
         db.Confession
+        .findById({ _id: req.params.id })
+        .then(dbRemoveConfession => dbRemoveConfession.remove())
+        .then(dbRemoveConfession => res.json(dbRemoveConfession))
+        .catch(err => res.status(422).json(err));
     }
 };
