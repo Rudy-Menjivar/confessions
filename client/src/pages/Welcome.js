6bde517bae1b81fd3842839ab45a5cd77bcb5a30
Madeline Morris <madehopemorr@gmail.com>
2021-03-13 12:39:08 -0800

    welcome page login

--- client/src/pages/Welcome.js
+++ client/src/pages/Welcome.js
@@ -1,12 +1,14 @@
 import React from "react";
 import NavBarWelcome from "../components/NavBarWelcome";
 import Jumbotron from "../components/Jumbotron";
+import LogIn from "../components/LogIn";
 
 function Welcome() {
     return (
         <div>
             <NavBarWelcome />
             <Jumbotron />
+            <LogIn />
             <h1>Welcome</h1>
         </div>
     );
