5b8eecfbad6df4d48d6faf0c02494904baaa5d4d
Madeline Morris <madehopemorr@gmail.com>
2021-03-13 10:49:51 -0800

    signup page

---
+++ client/src/pages/Signup.js
@@ -0,0 +1,15 @@
+import React from "react";
+import NavBarWelcome from "../components/NavBarMember";
+import SignUp from "../components/SignUp"
+
+function Signuppage() {
+    return (
+        <div>
+            <NavBarWelcome />
+            <SignUp />
+            <h1>Sign Up</h1>
+        </div>
+    );
+};
+
+export default Signuppage;
