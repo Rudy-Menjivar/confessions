991ab94629360558ff9f6fb197271ac11935ec82
Uyen Nguyen <udnguyen@ucsd.edu>
2021-03-14 00:42:01 -0800

    add logic for GET route to load posts in profile page

--- client/src/components/Profile/index.js
+++ client/src/components/Profile/index.js
@@ -1,23 +1,49 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import NavBarMember from "../NavBarMember";
+import API from "../../utils/API";
+import PostCard from "../PostCard";
 
 function Profile() {
+    // Setting initial state of confession posts
+    const [posts, setConfessions] = useState([]);
+
+    // Load all confessions and store them with setConfessions
+    useEffect(() => {
+        loadConfessions()
+    }, []);
+
+    // Loads all confession posts and set them to confessions
+    function loadConfessions() {
+        API.getConfessions()
+            .then(res =>
+                setConfessions(res.data)
+            )
+            .catch(err => console.log(err));
+    };
+
     return (
         <div>
             <NavBarMember />
             <section>
-                <p>Name: </p>
+                <p>Username: </p>
                 <p>Info: </p>
             </section>
             <section>
-                <ul>
-                    <ol>
-                        Post: Example
-                </ol>
-                </ul>
+                {posts.length ? (
+                    <div>
+                        {posts.map(post => (
+                            <PostCard key={post._id}>
+                                {post.content}
+                            </PostCard>
+                        ))}
+                    </div>
+                ) : (
+                    <h3>You have no post yet!</h3>
+                )}
             </section>
         </div>
     );
+
 };
 
 export default Profile;
