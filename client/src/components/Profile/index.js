9ef8b601229f1aa7c41c423ba4ba53840dfdbbb8
Uyen Nguyen <udnguyen@ucsd.edu>
2021-03-15 03:37:13 -0700

    sketch profile appearance, add input box to create new confession post, add Edit and Delete buttons

--- client/src/components/Profile/index.js
+++ client/src/components/Profile/index.js
@@ -1,16 +1,25 @@
 import React, { useEffect, useState } from "react";
+import "./style.css";
 import NavBarMember from "../NavBarMember";
 import API from "../../utils/API";
+import ProfileInfo from "../ProfileInfo";
+import PostCreateBox from "../PostCreateBox";
 import PostCard from "../PostCard";
 
 function Profile() {
     // Setting initial state of confession posts
+    // const [profileInfo, setProfileInfo] = useState([]);
     const [posts, setConfessions] = useState([]);
 
     // Load all confessions and store them with setConfessions
     useEffect(() => {
         loadConfessions()
     }, []);
+
+    // Load profile info for user who currently signed in
+    // function loadProfileInfo() {
+
+    // };
 
     // Loads all confession posts and set them to confessions
     function loadConfessions() {
@@ -24,23 +33,41 @@ function Profile() {
     return (
         <div>
             <NavBarMember />
-            <section>
-                <p>Username: </p>
-                <p>Info: </p>
-            </section>
-            <section>
-                {posts.length ? (
-                    <div>
-                        {posts.map(post => (
-                            <PostCard key={post._id}>
-                                {post.content}
-                            </PostCard>
-                        ))}
-                    </div>
-                ) : (
-                    <h3>You have no post yet!</h3>
-                )}
-            </section>
+            <div className="profile-container">
+                <h1>This page is private to you</h1>
+                <section>
+                    <ProfileInfo />
+                </section>
+                {/* <section>
+                {posts.map(post => (
+                    <ProfileInfo key={post._id}>
+                        {post.content}
+                    </ProfileInfo>
+                ))}
+            </section> */}
+                <section>
+                    <PostCreateBox />
+                </section>
+                <section>
+                    {posts.length ? (
+                        <div>
+                            {posts.map(post => (
+                                <PostCard key={post._id}>
+                                    <p>{post.content}</p>
+                                    <button>
+                                        Edit
+                                    </button>
+                                    <button>
+                                        Delete
+                                    </button>
+                                </PostCard>
+                            ))}
+                        </div>
+                    ) : (
+                        <h3>You have no post yet!</h3>
+                    )}
+                </section>
+            </div>
         </div>
     );
 
