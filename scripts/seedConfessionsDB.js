db6f13797925aff67290f4d5381bec84a2eff88e
Rudy Menjivar <rudy.menjivar@gmail.com>
2021-03-16 15:25:44 -0700

    Updated mongoDb connection, included config files with dotenv to load env variables, updated seeds along with mongoDb connection and fixed deprecated warning (replacing .remove for .deleteMany). dependancies added: bcrypt, passport, passport-local-mongoose

--- scripts/seedConfessionsDB.js
+++ scripts/seedConfessionsDB.js
@@ -1,43 +1,61 @@
 const mongoose = require("mongoose");
 const db = require("../models");
 
-mongoose.connect(
-  process.env.MONGODB_URI ||
-  "mongodb://localhost/confessions"
-)
+// next two lines load dot env variables (only needed for dev)
+const dotenv = require("dotenv")
+dotenv.config({ path: "./config/config.env" })
+
+const connectDB = async () => {
+  try {
+    const conn = await 
+    mongoose.connect(process.env.MONGO_URI || 
+      "mongodb://localhost/confessions", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+    });
+    console.log(`Seed records sent to: ${conn.connection.host}`)
+  } catch (err) {
+      console.error(err)
+      process.exit(1)
+  };
+};
+
+connectDB();
+
 const confessionSeed = [
-{
+  {
     title: "I skipped the gym",
-    content: "And ate chocolate with ice cream",
+    content: "I never worked out and instead destroyed the pizza, as well as the salted caramel chocolate ice cream",
     category: "Gym",
     likes: 3,
     reported: false
-},
-{
+  },
+  {
     title: "I cheated in school",
     content: "lorem Ipsum is Lorem Ipsum is crae cray",
     category: "School",
     likes: 2,
     reported: false
-},
-{
+  },
+  {
     title: "I lied to my mom",
-    content: "I think she knows!",
+    content: "I ate the entire costco bag of cheetos and I think she knows!",
     category: "Lies",
     likes: 5,
     reported: false
-},
+  },
 {
     title: "I skipped class",
-    content: "And watched a movie",
+    content: "I skipped on the project to watch a movie, go shopping and binge watch some more..",
     category: "School",
     likes: 0,
     reported: false
-}
+  }
 ];
 
 db.Confession
-  .remove({})
+  .deleteMany({})
   .then(() => db.Confession.collection.insertMany(confessionSeed))
   .then(data => {
     console.log(data.result.n + " records inserted!");
--- scripts/seedUsersDB.js
+++ scripts/seedUsersDB.js
@@ -1,23 +1,41 @@
 const mongoose = require("mongoose");
 const db = require("../models");
 
-mongoose.connect(
-  process.env.MONGODB_URI ||
-  "mongodb://localhost/confessions"
-)
+// next two lines load dot env variables (only needed for dev)
+const dotenv = require("dotenv")
+dotenv.config({ path: "./config/config.env" })
+
+const connectDB = async () => {
+  try {
+    const conn = await 
+    mongoose.connect(process.env.MONGO_URI || 
+      "mongodb://localhost/confessions", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+    });
+    console.log(`Seed records sent to: ${conn.connection.host}`)
+  } catch (err) {
+      console.error(err)
+      process.exit(1)
+  };
+};
+
+connectDB();
+
 const userSeed = [
-{
+  {
     userName: "whiteRabbit",
     password: "password"
-},
-{
+  },
+  {
     userName: "blackRabbit",
     password: "youwillneverfindouthaha"
-}
+  }
 ];
 
 db.User
-  .remove({})
+  .deleteMany({})
   .then(() => db.User.collection.insertMany(userSeed))
   .then(data => {
     console.log(data.result.n + " records inserted!");
