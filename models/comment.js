e743489e075de771b29c9ad8dcad9a562dc9dcdf
Madeline Morris <madehopemorr@gmail.com>
2021-03-16 14:13:35 -0700

    updated schema

--- models/comment.js
+++ models/comment.js
@@ -2,8 +2,9 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const CommentsSchema = new Schema({
-    user: String,
-    text: String
+    userName: { type: String, required: true },
+    text: { type: String, required: true },
+    date: { type: Data, default: Date.now }
 });
 
 module.exports = mongoose.model('Comment', CommentsSchema);
