d8bdce47b59e27e3e330acf49b04636277654658
Madeline Morris <madehopemorr@gmail.com>
2021-03-17 18:42:12 -0700

    confession model update

--- models/confession.js
+++ models/confession.js
@@ -1,15 +1,24 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const confessionSchema = new Schema({
-    title: {type: String, required: true },
-    content: {type: String, required: true},
-    likes: { type: Number },
-    comment: Array,
-    category: Array,
-    reported: { type: Boolean, default: false },
-    date: { type: Date, default: Date.now }
-});
+const confessionSchema = new Schema(
+    {
+        title: { type: String, required: true },
+        content: { type: String, required: true },
+        likes: { type: Number },
+        reported: { type: Boolean, default: false },
+        date: { type: Date, default: Date.now },
+        comments: [
+            {
+                type: String
+            }
+        ],
+        categories: [
+            {
+                type: String
+            }
+        ]
+    });
 
 const Confession = mongoose.model("Confession", confessionSchema);
 
