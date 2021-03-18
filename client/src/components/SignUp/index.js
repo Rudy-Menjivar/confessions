8452c9787a74ab472421f17133e70a59e754c05b
Madeline Morris <madehopemorr@gmail.com>
2021-03-17 19:49:40 -0700

    signup button

--- client/src/components/SignUp/index.js
+++ client/src/components/SignUp/index.js
@@ -3,27 +3,23 @@ import "./style.css";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 
-function SignUp() {
+function SignUp(props) {
     return (
         <Form>
             <Form.Group controlId="formBasicEmail">
                 <Form.Label>Email address</Form.Label>
-                <Form.Control type="email" placeholder="Please enter your email" />
+                <Form.Control {...props} type="email" placeholder="Please enter your email" />
                 <Form.Text className="text-muted">
                     We'll never share your email with anyone else
                 </Form.Text>
             </Form.Group>
-
+            <Form.Label>Username</Form.Label>
+            <Form.Control {...props} type="username" placeholder="Please enter your username" />
             <Form.Group controlId="formBasicPassword">
                 <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Password" />
+                <Form.Control {...props} type="password" placeholder="Password" />
             </Form.Group>
-
-            <Form.Group controlId="formBasicCheckbox">
-                <Form.Check type="checkbox" label="Check me out" />
-            </Form.Group>
-
-            <Button type="submit">Submit</Button>
+            <Button {...props} variant="primary" type="submit">Submit</Button>
         </Form>
     );
 };
