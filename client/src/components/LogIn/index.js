0fad23ab04289506d9063e7f0887a5f90745fc0f
Rudy Menjivar <rudy.menjivar@gmail.com>
2021-03-19 15:32:28 -0700

    Update index.js
    Corrected placeholder value to username

--- client/src/components/LogIn/index.js
+++ client/src/components/LogIn/index.js
@@ -8,7 +8,7 @@ function LogIn() {
         <Form>
             <Form.Group controlId="formGroupUsername">
                 <Form.Label>Username</Form.Label>
-                <Form.Control type="username" placeholder="password" />
+                <Form.Control type="username" placeholder="Username" />
             </Form.Group>
             <Form.Group controlId="formGroupPassword">
                 <Form.Label>Password</Form.Label>
