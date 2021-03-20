879ae8009075bf8e64199357246b652ec9c19a6b
Madeline Morris <madehopemorr@gmail.com>
2021-03-20 10:26:31 -0700

    api.js

--- client/src/utils/profileAPI.js
+++ client/src/utils/profileAPI.js
@@ -2,23 +2,23 @@ import axios from "axios";
 
 export default {
   // Gets all confession posts
-  getConfessions: function() {
+  getConfessions: function () {
     return axios.get("/api/profile");
   },
   // Gets the confession post with the given id
-  getConfession: function(id) {
+  getConfession: function (id) {
     return axios.get("/api/profile/" + id);
   },
   // Deletes the confession post with the given id
-  deleteConfession: function(id) {
+  deleteConfession: function (id) {
     return axios.delete("/api/profile/" + id);
   },
   // Saves a confession post to the database
-  saveConfession: function(confessionData) {
+  saveConfession: function (confessionData) {
     return axios.post("/api/profile", confessionData);
   },
   // Update a confession post with a given id
-  updateConfession: function(id) {
+  updateConfession: function (id) {
     return axios.put("/api/profile/" + id);
   }
 };
