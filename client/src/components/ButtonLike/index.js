cbecb505993af77df6d5031a189e0fb56c1e8847
Uyen Nguyen <udnguyen@ucsd.edu>
2021-03-20 12:00:13 -0700

    add logics for like button in profile page

--- client/src/components/ButtonLike/index.js
+++ client/src/components/ButtonLike/index.js
@@ -1,10 +1,26 @@
 import React from "react";
+import API from "../../utils/newsfeedAPI";
 import "./style.css";
 
 function LikeButton(props) {
+    const handleLikeClickEvent = (e) => {
+        const body = {
+            likes: parseInt(e.target.value) + 1
+        };
+        API.updateConfession(props.id, body)
+            .then(res => {
+                props.loadConfessions();
+            })
+            .catch(err => console.log(err));
+    };
+
     return (
-        <button {...props}></button>
-        // <button {...props} role="button">{props.children}</button>
+        <button
+            className={props.className}
+            value={props.value}
+            onClick={(e) => handleLikeClickEvent(e)}>
+            Likes: {props.value}
+        </button>
     );
 };
 
