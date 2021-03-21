1ccdcfb1b9e108099dddfcd838a95aae710e4c5d
Uyen Nguyen <udnguyen@ucsd.edu>
2021-03-21 00:31:37 -0700

    remove search bar and dropdown on navbar on welcome page

--- client/src/components/NavBarWelcome/index.js
+++ client/src/components/NavBarWelcome/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Navbar, Nav, NavDropdown, Form, FormControl, Button } from "react-bootstrap";
+import { Navbar, Nav, NavDropdown } from "react-bootstrap";
 
 
 function NavBarWelcome() {
@@ -14,16 +14,7 @@ function NavBarWelcome() {
                     <NavDropdown.Divider />
                     <Nav.Link href="/signup">Sign up</Nav.Link>
                     <NavDropdown.Divider />
-                    <NavDropdown title="Search" id="basic-nav-dropdown">
-                        <NavDropdown.Item href="#action/3.1">Example</NavDropdown.Item>
-                        <NavDropdown.Item href="#action/3.2">Another example</NavDropdown.Item>
-                        <NavDropdown.Item href="#action/3.3">Some other example</NavDropdown.Item>
-                    </NavDropdown>
                 </Nav>
-                <Form inline>
-                    <FormControl type="text" placeholder="Search..." className="mr-sm-2" />
-                    <Button variant="outline-success">Search</Button>
-                </Form>
             </Navbar.Collapse>
         </Navbar>
     );
