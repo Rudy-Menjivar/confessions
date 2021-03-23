e321a9dbd762da88d5ba08cbd0678a24f16de352
Uyen Nguyen <udnguyen@ucsd.edu>
2021-03-23 02:19:20 -0700

    remove the default react route that is not needed

--- routes/index.js
+++ routes/index.js
@@ -12,8 +12,8 @@ router.use("/api", apiRoutes);
 router.use("/api", userRoute);
 
 // If no API routes are hit, send the React app
-router.use(function(req, res) {
-    res.sendFile(path.join(__dirname, "../client/build/index.html"));
-});
+// router.use(function(req, res) {
+//     res.sendFile(path.join(__dirname, "../client/build/index.html"));
+// });
 
 module.exports = router;
