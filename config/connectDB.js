eb388939ae97d3fcfe65802c3b447c8ecda81356
Uyen Nguyen <udnguyen@ucsd.edu>
2021-03-24 22:17:17 -0700

    deploy

--- config/connectDB.js
+++ config/connectDB.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(
-      process.env.MONGO_URI || 
+      process.env.MONGODB_URI || 
       "mongodb://localhost/confessions", {
         useNewUrlParser: true,
         useUnifiedTopology: true,
